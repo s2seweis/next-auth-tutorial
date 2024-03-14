c2db43b2dd73eea5bfaa671a9e18a8d10e6bbb68
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-03-14 13:19:58 +0100

    next

---
+++ src/app/more/page.tsx
@@ -0,0 +1,12 @@
+export default async function MorePage() {
+
+    return (
+        <section className="flex flex-col gap-6">
+            <div>
+            {/* <div user={session?.user} pagetype={"Server"} /> */}
+            <h3>More More More</h3>
+            </div>
+        </section>
+    )
+
+}
